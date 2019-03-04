8a361288518bc4c663776ffc6697b47d77c3ed06
Luciano Braga <lucianobragaweb@gmail.com>
2019-03-04 19:04:47 -0300

    Implementando Filtrar Tasks

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -4,12 +4,15 @@ import {
 	View,
 	StyleSheet,
 	ImageBackground,
-	FlatList
+	FlatList,
+	TouchableOpacity,
+	Platform
 } from 'react-native'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import todayImage from '../../assets/imgs/today.jpg'
 import CommonStyles from '../CommonStyles'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Task from '../components/Task'
 
@@ -64,7 +67,26 @@ export default class Agenda extends React.Component {
 				estimateAt: new Date(),
 				doneAt: null
 			},
-		]
+		],
+		visibleTasks: [],
+		showTasksDone: true
+	}
+
+	filterTasks = () => {
+		let visibleTasks = null
+		if(this.state.showDoneTasks) {
+			visibleTasks = [...this.state.tasks]
+		} else {
+			const pendding = task => task.doneAt === null
+			visibleTasks = this.state.tasks.filter(pendding)
+		}
+		this.setState({ visibleTasks })
+	}
+
+	toggleFilter = () => {
+		this.setState({ showDoneTasks: !this.state.showDoneTasks },
+			this.filterTasks)
+
 	}
 
 	toggleTask = id => {
@@ -76,13 +98,24 @@ export default class Agenda extends React.Component {
 			return task
 		})
 
-		this.setState({ tasks })
+		this.setState({ tasks }, this.filterTasks)
+	}
+
+	componentDidMount = () => {
+		this.filterTasks()
 	}
 
 	render () {
 		return (
 			<View style={styles.container}>
 				<ImageBackground source={todayImage} style={styles.background}>
+					<View style={styles.iconBar}>
+						<TouchableOpacity onPress={this.toggleFilter}>
+							<Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+								size={20}
+								color={CommonStyles.colors.secondary} />
+						</TouchableOpacity>
+					</View>
 					<View style={styles.titleBar}>
 						<Text style={styles.title}>Hoje</Text>
 						<Text style={styles.subtitle}>
@@ -93,7 +126,7 @@ export default class Agenda extends React.Component {
 
 				<View style={styles.tasksContainer}>
 					<FlatList
-						data={this.state.tasks}
+						data={this.state.visibleTasks}
 						keyExtractor={item => `${item.id}`}
 						renderItem={({item}) =>
 							<Task { ...item } toggleTask={this.toggleTask} />} />
@@ -130,5 +163,11 @@ const styles = StyleSheet.create({
 	},
 	tasksContainer: {
 		flex: 7
+	},
+	iconBar: {
+		marginTop: Platform.OS === 'ios' ? 30 : 10,
+		marginHorizontal: 20,
+		flexDirection: 'row',
+		justifyContent: 'flex-end'
 	}
 })
