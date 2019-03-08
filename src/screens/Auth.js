d8ad8fcad24022fc351e670097364f66605e8150
Luciano Braga <lucianobragaweb@gmail.com>
2019-03-08 20:15:57 -0300

    Implementando Validação do formulário

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -23,38 +23,60 @@ export default class Auth extends React.Component {
 		confirmPassword: ''
 	}
 
-	signinOrSignup = async () => {
+	signin = async () => {
+		try{
+			const res = await axios.post(`${server}/signin`, {
+				email: this.state.email,
+				password: this.state.password
+			})
+
+			axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+
+			this.props.navigation.navigate('Home')
+		} catch (err) {
+			Alert.alert('Erro', 'Falha no Login!')
+		}
+
+	}
+
+	signup = async () => {
+		try{
+			await axios.post(`${server}/signup`, {
+				name: this.state.name,
+				email: this.state.email,
+				password: this.state.password,
+				confirmPassword: this.state.confirmPassword
+			})
+
+			Alert.alert('Sucesso!', 'Usuário Cadastrado ;)')
+			this.setState({ stageNew: false })
+		} catch(err) {
+			showError(err)
+		}
+	}
+
+	signinOrSignup = () => {
 		if(this.state.stageNew) {
-			try{
-				await axios.post(`${server}/signup`, {
-					name: this.state.name,
-					email: this.state.email,
-					password: this.state.password,
-					confirmPassword: this.state.confirmPassword
-				})
-
-				Alert.alert('Sucesso!', 'Usuário Cadastrado ;)')
-				this.setState({ stageNew: false })
-			} catch(err) {
-				showError(err)
-			}
+			this.signup()
 		} else {
-			try{
-				const res = await axios.post(`${server}/signin`, {
-					email: this.state.email,
-					password: this.state.password
-				})
-
-				axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-
-				this.props.navigation.navigate('Home')
-			} catch (err) {
-				Alert.alert('Erro', 'Falha no Login!')
-			}
+			this.signin()
 		}
 	}
 
 	render () {
+		const validations = []
+
+		validations.push(this.state.email && this.state.email.includes('@'))
+		validations.push(this.state.password && this.state.password.length >= 6)
+
+		if (this.state.stageNew){
+			validations.push(this.state.name && this.state.name.trim())
+			validations.push(this.state.confirmPassword)
+			validations.push(this.state.password === this.state.confirmPassword)
+		}
+
+		const validForm = validations.reduce((all, v) => all && v)
+
 		return (
 			<ImageBackground source={backgroundImage}
 				style={styles.background}>
@@ -90,8 +112,9 @@ export default class Auth extends React.Component {
 							value={this.state.confirmPassword}
 							onChangeText={confirmPassword => this.setState({ confirmPassword })} />}
 
-					<TouchableOpacity onPress={this.signinOrSignup}>
-						<View style={styles.button}>
+					<TouchableOpacity disabled={!validForm}
+						onPress={this.signinOrSignup}>
+						<View style={[styles.button, !validForm ? { backgroundColor: '#aaa'} : {}]}>
 							<Text style={styles.buttonText}>
 								{this.state.stageNew ? 'Registrar' : 'Entrar'}
 							</Text>
